eed7b5f86915da330704c2e31e795d598ddfda58
emanuelaguna <emanuelaguna@gmail.com>
2020-02-06 11:11:36 -0600

    fixing backend access key

--- terraform/App_service_plan.tf
+++ terraform/App_service_plan.tf
@@ -6,7 +6,7 @@ terraform {
         storage_account_name = "floappaccount"
         container_name       = "terraform"
         key                  = "terraform.tfstate"
-        access_key           = "DefaultEndpointsProtocol=https;AccountName=floappaccount;AccountKey=fHccV8GckC3WSbAS6IdC0J9A+ZqBREoweQ1LksqXMh+JjDVSW6zoDmoq0Rg0bSUflgXabw5WcGXhBcOaK4RsqQ==;EndpointSuffix=core.windows.net"
+        access_key           = "fHccV8GckC3WSbAS6IdC0J9A+ZqBREoweQ1LksqXMh+JjDVSW6zoDmoq0Rg0bSUflgXabw5WcGXhBcOaK4RsqQ=="
     }
 }
 
